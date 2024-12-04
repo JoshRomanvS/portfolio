c8e8acb8b3713a41a3c4225e9f94d3d76969cac5
Joshua van Schravendijk <joshua200312@outlook.com>
2024-12-04 17:55:02 +0100

    begin navbar

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Image from "next/image";
 
-export default function NavBar({ title }) {
-  return <h1>{title ? title : "Default title"}</h1>;
+
+export default function NavBar() {
+  return (
+    <div className="w-full h-20 sticky top-0">
+        <div className="container flex justify-between items-center h-full px-4">
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+
+            <p>hello</p>
+        </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Image from "next/image";
-import { NavBar } from "./header";
+import NavBar from "./header";
 
 
 export default function Page() {
   return (
     <div>
       <NavBar />
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
     </div>
   );
 }
